987670ed48595db421ddfde57aca5b0cec2ded21
Diego Parisi <gandaldf@users.noreply.github.com>
2019-06-13 17:20:52 +0200

    Update doc.go

--- doc.go
+++ doc.go
@@ -1,9 +1,7 @@
-/*
-Rpi
-Personal Golang libraries for some Raspberry Pi hardware.
-
-e-Papers:
-- epd7in5: 7.5inch e-paper (https://www.waveshare.com/wiki/7.5inch_e-Paper_HAT)
-*/
-
+// Rpi
+// Personal Golang libraries for some Raspberry Pi hardware.
+//
+// e-Papers:
+// - epd7in5: 7.5inch e-paper (https://www.waveshare.com/wiki/7.5inch_e-Paper_HAT)
+//
 package rpi
